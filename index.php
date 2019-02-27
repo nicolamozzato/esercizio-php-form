e9143445e45511ba6a4a06a1e426ee631b256642
nicolamozzato <39949202+nicolamozzato@users.noreply.github.com>
2019-02-27 09:18:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <!-- Decorazione della pagina -->
+        <link rel="stylesheet" href="css/style.css">
+
+
+        <title> </title>
+    </head>
+
+
+    <body>
+            
+        <div class="container-fluid">
+            <div class="row">
+            
+                <ul>
+        
+                         <?php
+                          $arr = ["arancia","carota","limone","pera","carota"]
+
+                          for ($=0; $ <count($A);$++) {
+
+                            print "<li>". $[1] </li>
+                          }
+               
+                </ul>
+
+            </div>
+        </div>
+
+
+
+	        <!-- Optional JavaScript -->
+	        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	        <script src="js/jquery-3.3.1.slim.min.js"></script>
+	        <script src="js/1.14.3/popper.min.js></script>
+	        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
